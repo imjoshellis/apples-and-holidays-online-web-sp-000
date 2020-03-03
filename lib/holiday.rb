b781ba24a2c23249264b6cdee1225778149d2b65
Josh Ellis <joshe523@gmail.com>
2020-03-03 06:23:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,7 +29,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash[:winter].each { |arr|
     binding.pry
-    arr << supply
+    arr[1] << supply
   }
 end
 
